f762c3b38825e929848228d84a597a3e4ecb4d9e
BaskaLemon <erdenebayarbaasandorj89@gmail.com>
2026-06-04 11:27:05 +0800

    fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,24 +3,35 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import pg from "pg";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient;
-  pool: pg.Pool;
+  prisma?: PrismaClient;
+  pool?: pg.Pool;
 };
 
-// Reuse the same pool
 const pool =
   globalForPrisma.pool ??
   new pg.Pool({
     connectionString: process.env.DATABASE_URL,
-    max: 5, // limit max connections
+
+    max: 10,
+
     idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
+
+    connectionTimeoutMillis: 15000,
   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.pool = pool;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.pool = pool;
+}
 
 const adapter = new PrismaPg(pool);
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+    log: ["error"],
+  });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
